490a3bcc602c2f7e0f5cb15c4c00522e9df913b5
mahfuz-7148 <mrahman7148@gmail.com>
2025-03-24 05:02:03 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ const App = () => {
 
                     {isLoading ? (
 
-                        <Spinner/>
+                           <Spinner/>
 
                     ) : errorMessage ? (
                         <p className="text-teal-400">{errorMessage}</p>
